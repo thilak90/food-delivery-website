2ca8ac397ebf3a9d2057f02b93864e02883e6330
Thilak BK <thilakbk@thestackly.com>
2026-08-17 13:23:13 +0530

    Add final React assignment features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
-import Home from './pages/Home'
-import {ToastContainer} from "react-toastify"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import FoodDetails from "./pages/FoodDetails";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
+import Checkout from "./pages/Checkout";
+import NotFound from "./pages/NotFound";
+import CategoryLayout from "./pages/CategoryLayout";
+import CategoryItems from "./pages/CategoryItems";
+import ApiFood from "./pages/ApiFood";
+
 function App() {
   return (
-    <div>
-     <Home/>
-    <ToastContainer/>
-    </div>
-  )
+    <>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/menu" element={<Menu />} />
+
+        <Route path="/menu/:id" element={<FoodDetails />} />
+
+        <Route path="/categories" element={<CategoryLayout />}>
+          <Route index element={<CategoryItems />} />
+          <Route path="breakfast" element={<CategoryItems category="breakfast" />} />
+          <Route path="soups" element={<CategoryItems category="soups" />} />
+          <Route path="pizza" element={<CategoryItems category="pizza" />} />
+          <Route path="burger" element={<CategoryItems category="burger" />} />
+          <Route path="pasta" element={<CategoryItems category="pasta" />} />
+        </Route>
+
+        <Route path="/api-food" element={<ApiFood />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/orders" element={<Orders />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <ToastContainer position="top-right" />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import UserContext from './context/UserContext.jsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
- <UserContext>
-    <App />
-    
-  </UserContext>
-  </Provider>
- 
-)
+import App from "./App.jsx";
+import UserContext from "./context/UserContext.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserContext>
+          <App />
+        </UserContext>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
